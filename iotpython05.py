1561c8f73c8ce397e4d9583ccc4bc96a9e54139d
Sumetnaja <Sumet471@gmail.com>
2023-09-08 12:30:00 +0700

    code v.1 : pythonproject.04

---
+++ iotpython05.py
@@ -0,0 +1,31 @@
+#หาพื้นที่สีเหลี่ยม และสามเหลี่ยม โดยรับกว้าง ยาว/ฐาน สูง ทางแป้นพิมพ์ และแสดงผลทางหน้าจอ
+# feature การทำงานอะไรบ้าง
+# 1. รับค่ากว้าง ยาว 2. รับค่า ฐานสูง 
+# 3. คำนวณพท.สี่เหลี่ยม และแสดงพท.สี่เหลี่ยม 4. คำนวณพท.สามเหลี่ยม และแสดงพท.สามเหลี่ยม
+def inputWitdhlong( ) :
+    wi = float( input('ป้อนกว้าง') )
+    lo = float( input('ป้อนยาว') )
+    return wi, lo
+
+def inputBaseHigh( ) :
+    ba = float( input('ป้อนฐาน : ') )
+    hi = float( input('ป้อนสูง : ') )
+    return ba, hi
+
+def calAndShowAreaSquare( wi, lo ) :
+    area = wi * lo
+    print(f'สี่เหลี่ยมกว้าง {wi} ยาว {lo} มีพื้นที่ {area}')
+
+def calAndShowAreaTrianble( ba, hi ) :
+    area = ba * hi / 2
+    print(f'สามเหลี่ยมฐาน {ba} สูง {hi} มีพื้นที่ {area}')
+
+wi, lo = inputWitdhlong()
+calAndShowAreaSquare(wi, lo)
+print('-----------------------')
+ba, hi = inputBaseHigh( )
+calAndShowAreaTrianble(ba, hi)
+
+
+
+
